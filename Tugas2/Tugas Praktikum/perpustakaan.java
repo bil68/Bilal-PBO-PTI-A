3b063eaed86606455c27898ea8a8c35e93ed3175
bil68 <161267850+bil68@users.noreply.github.com>
2024-03-14 19:50:56 +0700

    Create perpustakaan.java

---
+++ Tugas2/Tugas Praktikum/perpustakaan.java
@@ -0,0 +1,423 @@
+package coba;
+
+class perpustakaan {
+    static String namapenulis1="bilal";
+    static String namapenulis2="kelinci";
+    static String namapenulis3="kuda terbang";
+    static String namapenerbit1="heighest publisher";
+    static String namapenerbit2="the corp";
+    static String namaeditor1="ouiijaa";
+    static String namadesainer2="andik firmansyah";
+    
+    perpustakaan(){
+    }
+}
+
+ class teknologi {
+  String judul;
+  String penulis;
+  String penerbit;
+  String editor;
+  String desainer;
+  void buku33(){
+     penulis = perpustakaan.namapenulis1;
+     penerbit = perpustakaan.namapenerbit1;
+     editor = perpustakaan.namaeditor1;
+     desainer = perpustakaan.namadesainer2;
+     judul = "AZAB TEMEN SUKA MINTA PAP";
+     System.out.println("\nJudul               : "+judul+
+     "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+     +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+  }
+  void buku34(){
+    penulis = perpustakaan.namapenulis2;
+    penerbit = perpustakaan.namapenerbit1;
+    editor = perpustakaan.namaeditor1;
+    desainer = perpustakaan.namadesainer2;
+    judul = "teknik komputer jaringan";
+    System.out.println("\nJudul               : "+judul+
+    "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+    +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+ }
+ void buk35(){
+    penulis = perpustakaan.namapenulis1;
+    penerbit = perpustakaan.namapenerbit1;
+    editor = perpustakaan.namaeditor1;
+    desainer = perpustakaan.namadesainer2;
+    judul = "matematika diskrit";
+    System.out.println("\nJudul               : "+judul+
+    "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+    +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+ }
+ void buku36(){
+    penulis = perpustakaan.namapenulis1;
+    penerbit = perpustakaan.namapenerbit1;
+    editor = perpustakaan.namaeditor1;
+    desainer = perpustakaan.namadesainer2;
+    judul = "sistem digital";
+    System.out.println("\nJudul               : "+judul+
+    "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+    +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+ }
+ void buku037(){
+    penulis = perpustakaan.namapenulis1;
+    penerbit = perpustakaan.namapenerbit1;
+    editor = perpustakaan.namaeditor1;
+    desainer = perpustakaan.namadesainer2;
+    judul = "rekayassa perangkat lunak";
+    System.out.println("\nJudul               : "+judul+
+    "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+    +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+ }
+}
+class agama {
+    String judul;
+    String penulis;
+    String penerbit;
+    String editor;
+    String desainer;
+    void buku33(){
+       penulis = perpustakaan.namapenulis1;
+       penerbit = perpustakaan.namapenerbit1;
+       editor = perpustakaan.namaeditor1;
+       desainer = perpustakaan.namadesainer2;
+       judul = "Buku Pendidikan Agama Islam";
+       System.out.println("\nJudul               : "+judul+
+       "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+       +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+    }
+    void buku034(){
+      penulis = perpustakaan.namapenulis2;
+      penerbit = perpustakaan.namapenerbit2;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "Buku Pendidikan Agama Kristen";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+   void buku035(){
+      penulis = perpustakaan.namapenulis1;
+      penerbit = perpustakaan.namapenerbit1;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "Buku Pendidikan Agama Hindu";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+   void buku036(){
+      penulis = perpustakaan.namapenulis2;
+      penerbit = perpustakaan.namapenerbit1;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "Buku Pendidikan Agama Budhha";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+   void buku037(){
+      penulis = perpustakaan.namapenulis2;
+      penerbit = perpustakaan.namapenerbit1;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "Buku Pendidikan Agama Khong Hu Cu";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+  }
+  class filsafat {
+    String judul;
+    String penulis;
+    String penerbit;
+    String editor;
+    String desainer;
+    void buku33(){
+       penulis = perpustakaan.namapenulis1;
+       penerbit = perpustakaan.namapenerbit1;
+       editor = perpustakaan.namaeditor1;
+       desainer = perpustakaan.namadesainer2;
+       judul = "filsafat berencana 1";
+       System.out.println("\nJudul               : "+judul+
+       "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+       +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+    }
+    void buku34(){
+      penulis = perpustakaan.namapenulis2;
+      penerbit = perpustakaan.namapenerbit1;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "filsafat berencana 2";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+   void buku35(){
+      penulis = perpustakaan.namapenulis2;
+      penerbit = perpustakaan.namapenerbit1;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "filsafat berencana 3";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+   void buku36(){
+      penulis = perpustakaan.namapenulis2;
+      penerbit = perpustakaan.namapenerbit1;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "filsafat berencana 4";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+   void buku37(){
+      penulis = perpustakaan.namapenulis2;
+      penerbit = perpustakaan.namapenerbit1;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "filsafat berencana 5";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+  }
+  class sejarah {
+    String judul;
+    String penulis;
+    String penerbit;
+    String editor;
+    String desainer;
+    void buku33(){
+       penulis = perpustakaan.namapenulis1;
+       penerbit = perpustakaan.namapenerbit2;
+       editor = perpustakaan.namaeditor1;
+       desainer = perpustakaan.namadesainer2;
+       judul = "BUKU IPS KELAS 5";
+       System.out.println("\nJudul               : "+judul+
+       "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+       +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+    }
+    void buku34(){
+      penulis = perpustakaan.namapenulis2;
+      penerbit = perpustakaan.namapenerbit2;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "BUKU IPS KELAS 4";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+   void buku35(){
+      penulis = perpustakaan.namapenulis1;
+      penerbit = perpustakaan.namapenerbit2;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "BUKU IPS KELAS 3";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+   void buku36(){
+      penulis = perpustakaan.namapenulis1;
+      penerbit = perpustakaan.namapenerbit2;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "BUKU IPS KELAS 2";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+   void buku37(){
+      penulis = perpustakaan.namapenulis3;
+      penerbit = perpustakaan.namapenerbit2;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "BUKU IPS KELAS 1";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+  }
+  class politik {
+    String judul;
+    String penulis;
+    String penerbit;
+    String editor;
+    String desainer;
+    void buku33(){
+       penulis = perpustakaan.namapenulis3;
+       penerbit = perpustakaan.namapenerbit2;
+       editor = perpustakaan.namaeditor1;
+       desainer = perpustakaan.namadesainer2;
+       judul = "pendangan revolusi 1";
+       System.out.println("\nJudul               : "+judul+
+       "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+       +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+    }
+    void buku34(){
+      penulis = perpustakaan.namapenulis2;
+      penerbit = perpustakaan.namapenerbit2;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "pandangan revolusi 2";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+   void buku35(){
+      penulis = perpustakaan.namapenulis1;
+      penerbit = perpustakaan.namapenerbit2;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "pandangan revolusi 3";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+   void buku36(){
+      penulis = perpustakaan.namapenulis2;
+      penerbit = perpustakaan.namapenerbit2;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "pandangan revolusi 4";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+   void buku37(){
+      penulis = perpustakaan.namapenulis3;
+      penerbit = perpustakaan.namapenerbit1;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "pandangan revolusi 5";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+  }
+  class psikologi {
+    String judul;
+    String penulis;
+    String penerbit;
+    String editor;
+    String desainer;
+    void buku33(){
+       penulis = perpustakaan.namapenulis1;
+       penerbit = perpustakaan.namapenerbit2;
+       editor = perpustakaan.namaeditor1;
+       desainer = perpustakaan.namadesainer2;
+       judul = "psikologi anak";
+       System.out.println("\nJudul               : "+judul+
+       "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+       +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+    }
+    void buku34(){
+      penulis = perpustakaan.namapenulis2;
+      penerbit = perpustakaan.namapenerbit2;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "psikologi anak PART 2";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+   void buku35(){
+      penulis = perpustakaan.namapenulis2;
+      penerbit = perpustakaan.namapenerbit2;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "memahami konsep behaviorisme";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+   void buku36(){
+      penulis = perpustakaan.namapenulis2;
+      penerbit = perpustakaan.namapenerbit2;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "MEMAHAMI WANITA";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+   void buku37(){
+      penulis = perpustakaan.namapenulis3;
+      penerbit = perpustakaan.namapenerbit2;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "MEMAHAMI WANITA jilid 2";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+  }
+  class fiksi {
+    String judul;
+    String penulis;
+    String penerbit;
+    String editor;
+    String desainer;
+    void buku33(){
+       penulis = perpustakaan.namapenulis1;
+       penerbit = perpustakaan.namapenerbit1;
+       editor = perpustakaan.namaeditor1;
+       desainer = perpustakaan.namadesainer2;
+       judul = "KANCIL MENYEBRANG SUNGAI";
+       System.out.println("\nJudul               : "+judul+
+       "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+       +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+    }
+    void buku34(){
+      penulis = perpustakaan.namapenulis3;
+      penerbit = perpustakaan.namapenerbit1;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "KANCIL MENCURI TIMUN";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+   void buku35(){
+      penulis = perpustakaan.namapenulis3;
+      penerbit = perpustakaan.namapenerbit2;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "KANCIL TIDUR";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+   void buku36(){
+      penulis = perpustakaan.namapenulis3;
+      penerbit = perpustakaan.namapenerbit2;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "KANCIL MENCURI KANCIL";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+   void buku37(){
+      penulis = perpustakaan.namapenulis3;
+      penerbit = perpustakaan.namapenerbit2;
+      editor = perpustakaan.namaeditor1;
+      desainer = perpustakaan.namadesainer2;
+      judul = "TIMUN MENCURI KANCIL";
+      System.out.println("\nJudul               : "+judul+
+      "\nPenulis             : "+penulis+"\nPenerbit            : "+penerbit
+      +"\nEditor              : "+editor+"\nDesain Grafis       : "+desainer);
+   }
+  }
+/**
+ * perpustakaan
+ */
+public class mainperpustakaan {
+public static void main(String[] args) {
+   
+}
+    
+}
